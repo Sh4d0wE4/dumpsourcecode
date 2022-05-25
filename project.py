46a8e24c11b9fa7b2a202589edc77f8fa5fa26b3
Astatin <87228325+Sh4d0wE4@users.noreply.github.com>
2022-05-25 08:09:20 -0400

    Create project.py

---
+++ project.py
@@ -0,0 +1,57 @@
+# Programming by Sh4d0wE4
+# Herhangi bir websitenin kaynak kodunu dump edip, bir klasore kayıt eden python programi.
+# A python program that dumps the source code of any website and saves it to a folder.
+
+import requests
+import os
+import subprocess
+import time
+
+os.system("clear")
+
+def main():
+    url = input("Lutfen verisini cekmek istediginiz sitenin\nURL'sini girin: ")
+    request(url)
+    print("Program basariyla calisti..")
+
+def request(url):
+    try:
+        r = requests.get(url)
+        statuscode = r.status_code
+
+        if statuscode == 200:
+            print("HTTP 200 OK")
+            time.sleep(1)
+            print("Lutfen biraz bekleyin..")
+            time.sleep(2)
+            print("\n")
+
+            print("Veriler cekiliyor..")
+            time.sleep(2)
+            f = open("text.html", "w")
+            f.write(str(r.text))
+            f.close()
+            print("Veriler basariyla yazdirildi..")
+            time.sleep(1)
+            print("Dosya aciliyor...")
+            subprocess.run(['code', 'text.html'])
+            time.sleep(2)
+            print("Dosya basariyla acildi..")
+            time.sleep(2)
+            os.system("clear")
+        
+        if statuscode == 404:
+            print("İnternet sitesi bulunamadi.")
+        
+        if statuscode == 403:
+            print("İnternet sitesine erisim yetkiniz yok.")
+        
+        servererrors = [501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511, 512]
+        for server in servererrors:
+            if statuscode == server:
+                print("Sunucu hatasi !")
+    
+    except:
+        print("Bir hata olustu!")
+
+main()
